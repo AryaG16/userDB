76af60f1237a7811c5cfeeb779c69f5cfca9466f
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2024-01-18 16:14:44 +0530

    Styled the user-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserAddEditComponent } from './user-add-edit/user-add-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserListComponent, UserAddEditComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    UserAddEditComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
