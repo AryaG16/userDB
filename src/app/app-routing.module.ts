168c790abe29080f4d36d0af8c287d59bb848733
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2024-01-18 15:26:43 +0530

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddEditComponent } from './user-add-edit/user-add-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent,
+  },
+  {
+    path: 'add-user',
+    component: UserAddEditComponent,
+  },
+  {
+    path: 'edit-user/:id',
+    component: UserAddEditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
