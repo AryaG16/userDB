7dad8b0c1ec3d453fa874af71184064db9714ae9
Aryan Bhavsar <aryanbhavsar161202@gmail.com>
2024-01-18 16:14:29 +0530

    Add dummy userList

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent {
-
+  users = [
+    {
+      name: 'Aryan Bhavsar',
+      age: 20,
+      phone: 9824990033,
+      email: 'a@asdnhjb.com',
+      password: 'a2sdki2',
+    },
+    {
+      name: 'Nandini Bhavsar',
+      age: 10,
+      phone: 8765438221,
+      email: 'nand@bha.com',
+      password: 'Kis4hsd',
+    },
+  ];
 }
